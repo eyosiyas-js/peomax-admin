b3c6dc0a81727d5029537017a58ec5e44c35308c
CyberTalent <132589551+vemoz111@users.noreply.github.com>
2023-05-09 16:49:51 +0300

    Added restaurant route

--- routes/restaurantsRoute.js
+++ routes/restaurantsRoute.js
@@ -12,7 +12,6 @@ const {
   mkdirSync,
 } = require("fs");
 const { join } = require("path");
-// const uploadFile = require("../utils/upload.js");
 
 const storage = join(process.cwd(), "./uploads");
 const formats = [
@@ -36,8 +35,17 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const restaurants = await Restaurant.find({});
-    res.send(restaurants);
+    const count = parseInt(req.query.count) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const skip = (page - 1) * count;
+    const restaurantsCount = await Restaurant.countDocuments({});
+    const totalPages = Math.ceil(restaurantsCount / count);
+    const restaurants = await Restaurant.find({}).skip(skip).limit(count);
+    res.send({
+      page,
+      totalPages,
+      restaurants,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send({ error: "Error getting restaurants" });
@@ -95,13 +103,18 @@ router.get("/:id/related", async (req, res) => {
   }
 });
 
-router.post("/create", uploads.array("images", 5), async (req, res) => {
+router.post("/add", uploads.array("images", 5), async (req, res) => {
   try {
     const files = await req.files;
     let hasInvalidFile = false;
-    const dataUrls = await Promise.all(
+    const images = await Promise.all(
       files.map(async (file) => {
         const { path, mimetype } = file;
+        if (!formats.includes(mimetype)) {
+          unlinkSync(path);
+          hasInvalidFile = true;
+          return;
+        }
         const buffer = readFileSync(path);
         const base64 = buffer.toString("base64");
         unlinkSync(path);
@@ -109,14 +122,51 @@ router.post("/create", uploads.array("images", 5), async (req, res) => {
       })
     );
 
-    res.send({ dataUrls });
+    if (hasInvalidFile)
+      return res.status(400).send({ error: "Invalid file type detected" });
+
+    const {
+      name,
+      description,
+      location,
+      tables,
+      availableSpots,
+      totalSpots,
+      openingTime,
+      closingTime,
+      numReviews,
+      totalBooks,
+    } = req.body;
+
+    const restaurant = new Restaurant({
+      name: name,
+      description: description,
+      location: location,
+      branches: req.body.branches ? req.body.branches : [],
+      image: images[0],
+      images: images,
+      rating: req.body.rating ? req.body.rating : 0,
+      tables: tables,
+      availableSpots: availableSpots,
+      totalSpots: totalSpots,
+      totalSpots: totalBooks,
+      openingTime: openingTime,
+      closingTime: closingTime,
+      numReviews: numReviews,
+      totalBooks: totalBooks,
+      restaurantID: uid(16),
+    });
+
+    await restaurant.save();
+
+    res.send(restaurant);
   } catch (error) {
-    res.status(500).send({ error: "Error uploading images" });
+    res.status(500).send({ error: "Error adding restaurant" });
     console.log(error);
   }
 });
 
-router.post("/add", async (req, res) => {
+router.post("/create", async (req, res) => {
   try {
     const {
       name,
@@ -124,7 +174,6 @@ router.post("/add", async (req, res) => {
       location,
       image,
       images,
-      rating,
       tables,
       availableSpots,
       totalSpots,
@@ -141,7 +190,7 @@ router.post("/add", async (req, res) => {
       branches: req.body.branches ? req.body.branches : [],
       image: image,
       images: images,
-      rating: rating,
+      rating: req.body.rating ? req.body.rating : 0,
       tables: tables,
       availableSpots: availableSpots,
       totalSpots: totalSpots,
@@ -157,7 +206,7 @@ router.post("/add", async (req, res) => {
 
     res.send(restaurant.toObject());
   } catch (error) {
-    res.status(500).send({ error: "Error updating restaurant" });
+    res.status(500).send({ error: "Error adding restaurant" });
     console.log(error);
   }
 });

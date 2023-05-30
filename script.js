2e87101e0aa511453b737e4e3dac66ac0c7631d5
CyberTalent <132589551+vemoz111@users.noreply.github.com>
2023-05-30 22:23:15 +0300

    updated .gitignore

--- script.js
+++ script.js
@@ -3,28 +3,22 @@ const Hotel = require("./models/Hotel"); // Assuming the schema is defined in a 
 
 require("dotenv").config();
 
-mongoose.set("strictQuery", false);
-
 const mongo_url = process.env.mongo_url;
 
 mongoose.set("strictQuery", false);
 mongoose
   .connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(async () => {
-    try {
-      const hotels = await Hotel.find(); // Retrieve all hotels
-
-      hotels.forEach(async (hotel) => {
-        hotel.ID = hotel.hotelID; // Assign the value of hotelID to ID
-        hotel.hotelID = undefined; // Remove the hotelID field
-        await hotel.save(); // Save the updated hotel document
-      });
-
-      console.log("Hotel IDs updated successfully!");
-    } catch (error) {
-      console.error("Error updating hotel IDs:", error);
-    } finally {
-      mongoose.disconnect(); // Close the database connection
-    }
+  .then(() => {
+    Hotel.updateMany(
+      {},
+      { $rename: { hotelID: "ID" } },
+      { multi: true },
+      function (err, blocks) {
+        if (err) {
+          throw err;
+        }
+        console.log("done!");
+      }
+    );
   })
   .catch((err) => console.error(err));

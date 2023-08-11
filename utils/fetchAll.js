8dcf20e5390836ced0876ed6ffac51f9d37a2eff
CyberTalent <132589551+vemoz111@users.noreply.github.com>
2023-08-11 09:12:21 +0300

    Updated fetchAll

--- utils/fetchAll.js
+++ utils/fetchAll.js
@@ -4,24 +4,37 @@ const Bar = require("../models/Bar");
 const Club = require("../models/Club");
 
 async function fetchAll(managerID) {
-  const query = {
-    $or: [
-      { supervisors: { $in: [managerID] } },
-      { employees: { $in: [managerID] } },
-      { managerID },
-    ],
-  };
+  if (managerID) {
+    const query = {
+      $or: [
+        { supervisors: { $in: [managerID] } },
+        { employees: { $in: [managerID] } },
+        { managerID },
+      ],
+    };
 
-  const [bars, clubs, hotels, restaurants] = await Promise.all([
-    Bar.find(query),
-    Club.find(query),
-    Hotel.find(query),
-    Restaurant.find(query),
-  ]);
+    const [bars, clubs, hotels, restaurants] = await Promise.all([
+      Bar.find(query),
+      Club.find(query),
+      Hotel.find(query),
+      Restaurant.find(query),
+    ]);
 
-  const all = [...bars, ...clubs, ...hotels, ...restaurants];
+    const all = [...bars, ...clubs, ...hotels, ...restaurants];
 
-  return all;
+    return all;
+  } else {
+    const [bars, clubs, hotels, restaurants] = await Promise.all([
+      Bar.find({ status: "approved" }),
+      Club.find({ status: "approved" }),
+      Hotel.find({ status: "approved" }),
+      Restaurant.find({ status: "approved" }),
+    ]);
+
+    const all = [...bars, ...clubs, ...hotels, ...restaurants];
+
+    return all;
+  }
 }
 
 module.exports = fetchAll;

f2f906eba61ae7a656dce00c2fb06b39cf7d0412
CyberTalent <132589551+vemoz111@users.noreply.github.com>
2023-08-11 10:13:57 +0300

    Auto rank assignment

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const Ticket = require("./models/Ticket");
 const User = require("./models/User");
+const fetchAll = require("./utils/fetchAll");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -12,22 +12,18 @@ mongoose
   .then(async () => {
     console.log("MongoDB connected!");
 
-    const tickets = await Ticket.find({});
+    const all = await fetchAll();
+    console.log(all.length);
 
-    for (let i = 0; i < tickets.length; i++) {
-      const ticket = tickets[i];
-      const user = await User.findOne({ userID: ticket.userID });
-      if (!user) {
-        await ticket.remove();
-      } else {
-        ticket.firstName = user.firstName;
-        ticket.lastName = user.lastName;
-        ticket.email = user.email;
-        ticket.phoneNumber = "+251 911 22 33 44";
+    for (let i = 0; i < all.length; i++) {
+      const item = all[i];
 
-        await ticket.save();
-      }
-      console.log(`ticket ${i} updated`);
+      item.rank = i + 1;
+      await item.save();
+
+      console.log(`${item.name} ranked: ${i}`);
     }
+
+    console.log(`Update complete`);
   })
   .catch((err) => console.error(err));

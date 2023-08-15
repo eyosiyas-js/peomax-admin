29fe55fbcba9a19551ddbb3b8513d841f1d6183a
CyberTalent <132589551+vemoz111@users.noreply.github.com>
2023-08-15 18:27:58 +0300

    Tickets /all

--- routes/ticketsRoute.js
+++ routes/ticketsRoute.js
@@ -3,9 +3,52 @@ const Event = require("../models/Event");
 const Ticket = require("../models/Ticket");
 const employeeChecker = require("../middleware/employeeChecker");
 const findPlace = require("../utils/findPlace.js");
+const fetchAll = require("../utils/fetchAll");
 const checkAuthorization = require("../utils/checkAuthorization.js");
 
 const router = express.Router();
+
+router.get("/all", employeeChecker, async (req, res) => {
+  try {
+    const count = parseInt(req.query.count) || 20;
+    const page = parseInt(req.query.page) || 1;
+
+    const all = await fetchAll(req.user.userID);
+
+    const startIndex = (page - 1) * count;
+
+    const matchQuery = { ID: { $in: all.map((item) => item.ID) } };
+    if (
+      req.query.status &&
+      (req.query.status == "expired" || req.query.status == "attended")
+    ) {
+      matchQuery.status = req.query.status;
+    }
+
+    const matchStage = { $match: matchQuery };
+
+    const sortStage = { $sort: { createdAt: -1 } };
+    const skipStage = { $skip: startIndex };
+    const limitStage = { $limit: count };
+
+    const [reservations, totalCount] = await Promise.all([
+      Ticket.aggregate([matchStage, sortStage, skipStage, limitStage]),
+      Ticket.aggregate([matchStage, { $count: "totalCount" }]),
+    ]);
+
+    const totalPages = Math.ceil(totalCount[0]?.totalCount / count) || 1;
+
+    res.send({
+      page,
+      totalPages,
+      reservationsCount: totalCount[0]?.totalCount || 0,
+      reservations,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Error getting reservations" });
+  }
+});
 
 router.get("/:id", employeeChecker, async (req, res) => {
   try {

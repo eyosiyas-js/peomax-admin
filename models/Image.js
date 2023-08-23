21bb4687ac731a7ac348fa7d86da49764d001336
CyberTalent <132589551+vemoz111@users.noreply.github.com>
2023-08-23 15:23:22 +0300

    local image storage in progress

---
+++ models/Image.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const imageSchema = new mongoose.Schema({
+  ID: {
+    type: String,
+    required: true,
+  },
+  data: {
+    type: Buffer,
+    required: true,
+  },
+  contentType: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Image = mongoose.model("Image", imageSchema);
+
+module.exports = Image;
---
+++ routes/imageRoute.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const Image = require("../models/Image");
+
+router.get("/:id", async (req, res) => {
+  try {
+    const image = await Image.findOne({ ID: req.params.ID });
+
+    if (!image) return res.status(404).send({ error: "Image not found" });
+
+    res.contentType(image.contentType);
+    res.send(image.data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ error: "Error getting image" });
+  }
+});
+
+module.exports = router;

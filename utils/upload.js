6c9ca435b7cecf7e545ce3129cddbbcf4d26d3be
CyberTalent <132589551+vemoz111@users.noreply.github.com>
2023-08-25 19:20:39 +0300

    Reverted img upload

--- utils/upload.js
+++ utils/upload.js
@@ -1,21 +1,30 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("../serviceAccountKey.json");
 const { uid } = require("uid");
-const Image = require("../models/Image");
-const fs = require('fs');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "gs://reserve-et.appspot.com",
+});
+
+const bucket = admin.storage().bucket();
 
 async function uploadFile(filePath, fileName, fileType) {
   try {
-    const name = fileName+uid(6);
-    const data = fs.readFileSync(filePath);
+    const destination = `/images/${uid(6)}${fileName}`;
 
-    const image = new Image({
-      ID: name,
-      contentType: fileType,
-      data: data
+    await bucket.upload(filePath, {
+      destination: destination,
+      metadata: {
+        contentType: fileType,
+      },
     });
 
-    await image.save();
-
-    const url = `https://api.peomax.com/images/${name}`;
+    const file = bucket.file(destination);
+    const [url] = await file.getSignedUrl({
+      action: "read",
+      expires: "03-17-2030",
+    });
     return { status: "success", url: url };
   } catch (error) {
     console.log(error);

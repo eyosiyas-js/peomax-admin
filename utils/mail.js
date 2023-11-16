dce010a895634f2b909a421a693e388ce3c079dc
Amieldev <amieldev@github.com>
2023-11-16 20:30:27 +0300

    smtp fix

--- utils/mail.js
+++ utils/mail.js
@@ -42,7 +42,7 @@ async function sendEmail(clientName, clientEmail, subject, verificationCode) {
   modifiedEmail = modifiedEmail.replace("{{code}}", verificationCode);
 
   let transporter = nodemailer.createTransport({
-    from: "infopeomax",
+    service: "smtp.gmail.com",
     port: 587,
     auth: {
       user: process.env.email,
